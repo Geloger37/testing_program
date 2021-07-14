4391e5e31cf074fc64c3f02b1093e91b25ab0662
Geloger37 <maks_shirokov_2000@mail.ru>
2021-07-14 21:11:43 +0800

    app

--- front/src/App.js
+++ front/src/App.js
@@ -2,26 +2,8 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and Windows!
-        </a>
-      </header>
->>>>>>> 9a1f89210368cd5fabcbbfd9690fe1cd36902460
     </div>
   );
 }
